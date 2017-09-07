d383813c892ab8c0b0502a8e027df97b3cc86a98
eventpb <59030195@kmitl.ac.th>
2017-09-07 21:35:43 +0700

    W04P01

--- Week04/Program.cs
+++ Week04/Program.cs
@@ -1,15 +1,11 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Week04
+namespace Lab01
 {
     class Program
     {
         static void Main(string[] args)
         {
+            Console.Write("Hello");
         }
     }
 }
